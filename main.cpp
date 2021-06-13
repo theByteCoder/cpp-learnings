0cea21629d72ee17472c70c833399c665cc96a9c
theByteCoder <47722399+theByteCoder@users.noreply.github.com>
2021-06-13 12:25:03 +0530

    feat(core): Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string name = "Subh";
+    int age = 29;
+    age = 30;
+    cout << "Hello world!" << endl;
+    cout << "This is " << name << ", glad to have you here" << endl;
+    name = "Subahsish Ghosh";
+    cout << "My full name is " << name << endl;
+    cout << "My age is "  << age<< endl;
+    return 0;
+}
