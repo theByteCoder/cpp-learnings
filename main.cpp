b91fbb9bdeeb1818a440408a93da53a3d9a66997
theByteCoder <47722399+theByteCoder@users.noreply.github.com>
2021-06-14 17:38:22 +0530

    feat(core): Updated math funtions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 // global scope
 string name = "Subh";
 int integers = -20;
-double doub = 0.12312;
+double doub = 2.1312;
 float flo = .0123123123;
 
 int main()
@@ -26,13 +27,24 @@ int main()
     name[0] = 'A';
     cout << "Modified name is " << name << endl;
 
+    // get substring
+    string subs = name.substr(0, 3);
+    cout << "sub string is " << subs << endl;
+
     // find string
     int index = name.find("Ghosh");
     cout << "string starts at " << index << endl;
 
-    // get substring
-    string subs = name.substr(0, 3);
-    cout << "sub string is " << subs << endl;
+    // math functions
+    cout << 3 + 2 << endl;
+    cout << 3 % 2 << endl;
+    cout << 2 % 2 << endl;
+    integers += doub;
+    cout << integers;
+    cout << floor(1.46);
+    cout << round(1.46);
+    cout << ceil(1.46);
+
 
     return 0;
 }
